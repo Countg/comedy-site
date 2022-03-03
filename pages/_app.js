3ae98e281949b8dbbbaa5ef023f64128510c1162
Gavin Stephens <gstephens090@gmail.com>
2022-03-03 14:16:22 -0500

    another commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import '../styles/globals.css';
 import { PodProvider } from '../providers/PodcastProvider';
-import { EventProvider } from '../providers/EventProvider';
 
 function MyApp({ Component, pageProps }) {
   return (
     <PodProvider>
-      <EventProvider>
-        <Component {...pageProps} />
-      </EventProvider>
+      <Component {...pageProps} />
     </PodProvider>
   );
 }
