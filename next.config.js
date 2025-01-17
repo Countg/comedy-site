4fda570bfbf8811f6fa92a158f1a0694aee283f3
Gavin Stephens <gstephens090@gmail.com>
2025-01-17 14:19:50 -0500

    new mailchimp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+
+  // Place swcMinify inside the compiler object
+  compiler: {
+    // swcMinify: true, // Enable SWC minification
+  },
+
+  // You can add any other configuration options here, if needed
 };
 
 export default nextConfig;
